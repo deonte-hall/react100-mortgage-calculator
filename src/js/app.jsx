57bcd0d062524e419e5687c1bc2b7dee99d58a77
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-05-27 14:44:56 -0700

    Initial framework completed and classes have been started

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 
 export default class App extends React.Component {
   // your Javascript goes here
-  render() {
+  constructor(props, balance, rate, term){
+    super(props);
+    this.balance = balance;
+    this.rate = rate;
+    this.term = term;
+  }
+  
+  statusChange(){
+    console.log("status changed");
+  }
+  render(){
     return (
       <div className='container'>
         {
-          
-          <h1>Hello World</h1>
-      
-        
+          <div className='user-input'>
+          <h3>Mortgage Calculator</h3>
+              <input type="number" name='balance' id='balance' value={this.balance} onChange="statusChange()"/>            
+              <input type="number" name='rate' id='rate' step={0.01} value={this.rate} onChange="statusChange()"/>
+              <select name="term" id="term" value={this.term} onChange='statusChange()'></select>
+              <button name='submit' id='submit' onClick={this.statusChange()}>Submit</button>
+            <div className='response-container' id='output'>
+            </div>
+          </div>                                   
         }
       </div>
-    );
+    )
   }
 }
