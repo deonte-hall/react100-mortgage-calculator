dd7675b940c0bbc9fcca32c0baab393549b5da44
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-05-31 16:25:48 -0700

    Updated state values --error in changing state with U/I

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,33 +3,37 @@ import React from 'react';
 
 export default class App extends React.Component {
   // your Javascript goes here
-  constructor(){
+  constructor(balance, term, rate){
     super();
+    this.balance = balance;
+    this.term = term;
+    this.rate = rate;
     this.state = {
-      hasBeenClicked: false,
-      balance: null,
-      term: null,
-      rate: null
+      balance: undefined,
+      term: undefined,
+      rate: undefined
     };
   }
   
-  statusChange(balance){
-    console.log(balance)
-  }
+  // statusChange(){
+  //   this.setState({
+  //     balance: document.getElementById('balance'),
+  //     term: document.getElementById('term'),
+  //     rate: document.getElementById('rate')
+  //   })
 
-  calculate(balance, rate, term){
+  // }
 
-  }
   render(){
     return (
       <div className='container'>
         {
           <div className='user-input'>
           <h3>Mortgage Calculator</h3>
-              <input type="number" name='balance' id='balance' value={this.balance} onChange={this.statusChange}/>            
-              <input type="number" name='rate' id='rate' step={0.01} value={this.rate} onChange={this.statusChange}/>
-              <select name="term" id="term" value={this.term} onChange={this.statusChange}></select>
-              <button name='submit' id='submit' onClick={this.statusChange()}>Submit</button>
+              <input type="number" name='balance' id='balance' value={this.balance} onChange={statusChange()}/>            
+              <input type="number" name='rate' id='rate' step={0.01} value={this.rate} onChange={statusChange()}/>
+              <select name="term" id="term" value={this.term} onChange={statusChange()}></select>
+              <button name='submit' id='submit' onClick={statusChange()}>Submit</button>
             <div className='response-container' id='output'>
 
             </div>
@@ -38,4 +42,8 @@ export default class App extends React.Component {
       </div>
     )
   }
+}
+
+let setState = () =>{
+  console.log("worked")
 }
