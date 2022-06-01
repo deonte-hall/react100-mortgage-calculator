d0bb2598acf7f6665828e6ae78c9f9e6aaec04dd
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-06-01 11:53:18 -0700

    Adds labels, and sets state values to the proper value , bindings needed

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,38 +3,93 @@ import React from 'react';
 
 export default class App extends React.Component {
   // your Javascript goes here
-  constructor(balance, term, rate){
-    super();
-    this.balance = balance;
-    this.term = term;
-    this.rate = rate;
+  constructor(props){
+    super (props);
     this.state = {
-      balance: undefined,
-      term: undefined,
-      rate: undefined
+      balance: 0,
+      rate: 0.00,
+      term: "15",
+      output: " " 
     };
+
   }
-  render(){
+
+    handleChange(event) {
+      this.setState({
+        balance : document.getElementById("")
+      });
+    }
+
+    
+
+    calculate(balance, rate, term) {
+      //M is your monthly payment.
+      //P is your principal.
+      //r is your monthly interest rate, calculated by dividing your annual interest rate by 12.
+      //n is your number of payments (the number of months you will be paying the loan).
+      const p = this.state.balance;
+      const r = (this.state.rate/100)/12;
+      const n = this.state.term * 12;
+    }
+    handleClick(){
+      const result = this.calculate();
+      this.setState ({
+        output: `${result.toFixed(2)} is your monthly payment!` 
+      });
+    }
+
+  render() {
     return (
       <div className='container'>
-        {
-          <div className='user-input'>
-          <h3>Mortgage Calculator</h3>
-              <input type="number" name='balance' id='balance' value={this.balance} onInput={setState}/>            
-              <input type="number" name='rate' id='rate' step={0.01} value={this.rate} onInput={setState}/>
-              <select name="term" id="term" value={this.term} onInput={setState}></select>
-              <button name='submit' id='submit' onInput={setState}>Submit</button>
-            <div className='response-container' id='output'>
-
+        <h3>My Mortgage Calculator</h3>
+        <form className="form-horizontal">
+          <div className="form-group">
+            {/* <label htmlFor="loanBalance" className="col-sm-2 control-label">Loan Balance ($USD)</label> */}
+            <div className="col-sm-10\">
+              <h3>Loan Balance:</h3>
+            {/*input loan balance*/}
+            <input onChange={this.handleChange} value={this.state.balance} 
+            type="number" 
+            name="balance" 
+            id ="loanBalance" 
+            placeholder="Enter Loan Balance"/>
             </div>
-          </div>                                   
-        }
+          </div>
+          < div className="form-group">
+           <h3>Interest Rate</h3>
+            {/*input interest rate*/}
+            <input onChange={this.handleChange} 
+            value={this.state.rate} 
+            name="rate" 
+            type="number" 
+            step=".01" 
+            id="interestRate" 
+            placeholder="Enter APR"/>
+          </div>
+          <div className="form-group">   
+            <h3>Loan Term:</h3>             
+                {/*select loan term in years*/} 
+                  <select onChange={this.handleChange} 
+                  value={this.state.term} 
+                  name="term" 
+                  id="loanTerm">
+                    <option value="15">15</option>
+                    <option value="30">30</option>
+                  </select>
+          </div>
+          <div className="form-group">
+            <div className="submit">
+            <button name="submit"
+            onClick={(e) => {
+              this.handleClick(e);
+            }}>Calculate</button>
+            </div>
+          </div>
+          <div name="output" 
+          id="output">{this.state.output}</div>
+        </form>
       </div>
-    )
-  }
-}
 
-let setState = () =>{
-  
-  console.log("state -changed to")
+    );
+  }
 }
