8bee8f1fec019fcac4249a366cb221bd1a65d849
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-06-01 11:19:43 -0700

    Additional state changes and an onChange handler to execute during input. Currently trying to link U/I with logic

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -14,26 +14,16 @@ export default class App extends React.Component {
       rate: undefined
     };
   }
-  
-  // statusChange(){
-  //   this.setState({
-  //     balance: document.getElementById('balance'),
-  //     term: document.getElementById('term'),
-  //     rate: document.getElementById('rate')
-  //   })
-
-  // }
-
   render(){
     return (
       <div className='container'>
         {
           <div className='user-input'>
           <h3>Mortgage Calculator</h3>
-              <input type="number" name='balance' id='balance' value={this.balance} onChange={statusChange()}/>            
-              <input type="number" name='rate' id='rate' step={0.01} value={this.rate} onChange={statusChange()}/>
-              <select name="term" id="term" value={this.term} onChange={statusChange()}></select>
-              <button name='submit' id='submit' onClick={statusChange()}>Submit</button>
+              <input type="number" name='balance' id='balance' value={this.balance} onInput={setState}/>            
+              <input type="number" name='rate' id='rate' step={0.01} value={this.rate} onInput={setState}/>
+              <select name="term" id="term" value={this.term} onInput={setState}></select>
+              <button name='submit' id='submit' onInput={setState}>Submit</button>
             <div className='response-container' id='output'>
 
             </div>
@@ -45,5 +35,6 @@ export default class App extends React.Component {
 }
 
 let setState = () =>{
-  console.log("worked")
+  
+  console.log("state -changed to")
 }
